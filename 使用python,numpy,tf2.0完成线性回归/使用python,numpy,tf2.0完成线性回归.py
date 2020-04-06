f299310e4a0219ea90776ec41b5373dee74c880e
syuctzhaoyang <48762502+syuctzhaoyang@users.noreply.github.com>
2020-04-06 10:24:46 +0800

    Update 使用python,numpy,tf2.0完成线性回归.py

--- 使用python,numpy,tf2.0完成线性回归/使用python,numpy,tf2.0完成线性回归.py
+++ 使用python,numpy,tf2.0完成线性回归/使用python,numpy,tf2.0完成线性回归.py
@@ -1,72 +1,113 @@
-x = [137.97, 104.50, 100.00, 124.32, 79.20, 99.00, 124.00, 114.00,
-     106.69, 138.05, 53.75, 46.91, 68.00, 63.02, 81.26, 86.21]
-y = [145.00, 110.00, 93.00, 116.00, 65.32, 104.00, 118.00, 91.00,
-     62.00, 133.00, 51.00, 45.00, 78.50, 69.65, 75.69, 95.30]
-x_test = [128.15, 45.00, 141.43, 106.27, 99.00, 53.84, 85.36, 70.00]
-# ---------------------------------------------------------
-# 1.使用纯python代码完成线性回归
-# meanX = sum(x) / len(x)
-# meanY = sum(y) / len(y)
-#
-# sumXY = 0.0
-# sumX = 0.0
-#
-# for i in range(len(x)):
-#     sumXY += (x[i] - meanX) * (y[i] - meanY)
-#     sumX += (x[i] - meanX) * (x[i] - meanX)
-#
-# w = sumXY / sumX
-# b = meanY - w * meanX
-#
-# print('w = ', w)
-# print('b=', b)
-#
-# for i in range(len(x_test)):
-#     print(x_test[i], '\t', w * x_test[i] + b)
+# 1.使用纯python语言完成最小二乘法线性回归
 
-# -------------------------------------------------------
-# 2.使用numpy 完成线性回归
+# a = 0
+# b = 0
+#
+# def f(x):
+#     y_pred = a * x + b
+#     return y_pred
+#
+# def loss(x, y):
+#     l = (a * x + b - y) ** 2
+#     return l
+#
+# def gradient_loss(x, y):
+#     g_a = 2 * (a * x + b - y) * x
+#     g_b = 2 * (a * x + b - y)
+#     return g_a, g_b
+#
+# X_raw = [2013, 2014, 2015, 2016, 2017]
+# Y_raw = [12000, 14000, 15000, 16500, 17500]
+# x_pred_raw = 2018
+# X = [(x - min(X_raw)) / (max(X_raw) - min(X_raw)) for x in X_raw]
+# Y = [(y - min(Y_raw)) / (max(Y_raw) - min(Y_raw)) for y in Y_raw]
+#
+# num_epoch = 10000
+# learning_rate = 1e-3
+# for e in range(num_epoch):
+#     for i in range(len(X)):
+#         x, y = X[i], Y[i]
+#         g_a, g_b = gradient_loss(x, y)
+#         a = a - learning_rate * g_a
+#         b = b - learning_rate * g_b
+# print(a, b)
+# for i in range(len(X)):
+#     x, y = X[i], Y[i]
+#     print(f(x), y)
+# x_pred = (x_pred_raw - min(X_raw)) / (max(X_raw) - min(X_raw))
+# y_pred = f(x_pred)
+# y_pred_raw = y_pred * (max(Y_raw) - min(Y_raw)) + min(Y_raw)
+# print(x_pred_raw, y_pred_raw)
 
+
+
+# 2.使用numpy 完成完成最小二乘法线性回归
 # import numpy as np
-# x = np.array(x)
-# y = np.array(y)
-# x_test = np.array(x_test)
 #
-# sumXY = np.sum((x - np.mean(x)) * (y - np.mean(y)))
-# sumX = np.sum((x - np.mean(x)) * (x - np.mean(x)))
+# X_raw = np.array([2013, 2014, 2015, 2016, 2017], dtype=np.float32)
+# y_raw = np.array([12000, 14000, 15000, 16500, 17500], dtype=np.float32)
 #
-# w = sumXY / sumX
-# b = np.mean(y) - w * np.mean(x)
+# X = (X_raw - X_raw.min()) / (X_raw.max() - X_raw.min())
+# y = (y_raw - y_raw.min()) / (y_raw.max() - y_raw.min())
 #
-# print('w = ', w)
-# print('b=', b)
+# a, b = 0, 0
 #
-# y_pred = w * x_test + b
-# for i in range(len(x_test)):
-#     print(x_test[i], '\t', np.round(y_pred[i],2))
+# num_epoch = 10000
+# learning_rate = 1e-3
+# for e in range(num_epoch):
+#     # 手动计算损失函数关于自变量（模型参数）的梯度
+#     y_pred = a * X + b
+#     grad_a, grad_b = (y_pred - y).dot(X), (y_pred - y).sum()
+#
+#     # 更新参数
+#     a, b = a - learning_rate * grad_a, b - learning_rate * grad_b
+#
+# print(a, b)
 
-# 3. 使用tensorflow完成线性回归
-# -----------------------------------------------
+# 3.使用tensorflow 2.0完成完成最小二乘法线性回归
+# _*_ coding: utf-8 _*_
+import numpy as np
 import tensorflow as tf
 
-tf.enable_eager_execution()
-x = tf.convert_to_tensor(x)
-y = tf.convert_to_tensor(y)
+X_raw = np.array([2013, 2014, 2015, 2016, 2017], dtype=np.float32)
+y_raw = np.array([12000, 14000, 15000, 16500, 17000], dtype=np.float32)
+X = (X_raw - X_raw.min()) / (X_raw.max() - X_raw.min())
+y = (y_raw - y_raw.min()) / (y_raw.max() - y_raw.min())
+print(X)
+print(y)
 
-x_test = tf.convert_to_tensor(x_test)
+X = tf.constant(X)
+y = tf.constant(y)
 
-meanX = tf.reduce_mean(x)
-meanY = tf.reduce_mean(y)
+w0 = tf.Variable(initial_value=0.)
+w1 = tf.Variable(initial_value=0.)
+variables = [w0, w1]
 
-sumXY = tf.reduce_sum((x - meanX) * (y - meanY))
-sumX = tf.reduce_sum((x - meanX) * (x - meanX))
+num_epoch = 1000
+optimizer = tf.keras.optimizers.SGD(learning_rate=1e-3)
+for e in range(num_epoch):
+    # 使用tf.GradientTape()记录损失函数的梯度信息
+    with tf.GradientTape() as tape:
+        y_pred = 0
+        for i in range(len(variables)):
+            y_pred += pow(X,i) * variables[i]
+        loss = 0.5 * tf.reduce_sum(tf.square(y_pred - y))
+    # TensorFlow自动计算损失函数关于自变量（模型参数）的梯度
+    grads = tape.gradient(loss, variables)
+    # TensorFlow自动根据梯度更新参数
+    optimizer.apply_gradients(grads_and_vars=zip(grads, variables))
 
-w = sumXY / sumX
-b = meanY - w * meanX
 
-print('w = ', w.numpy())
-print('b=', b.numpy())
+x_test = 2019
+X = (X_raw - X_raw.min()) / (X_raw.max() - X_raw.min())
+y = 0
+for i in range(len(variables)):
+    y += pow(X, i) * variables[i]
 
-y_pred = w * x_test + b
-for i in range(len(x_test)):
-    print(x_test.numpy()[i], '\t', y_pred.numpy()[i])
+y = (y_raw.max() - y_raw.min()) * y + y_raw.min()
+print(y)
+import matplotlib.pyplot as plt
+
+plt.scatter(X_raw,y_raw)
+plt.plot(X_raw,y,c='r')
+plt.show()
