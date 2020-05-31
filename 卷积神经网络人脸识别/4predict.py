a5be7a9ff9419d025db449550c995ff57043ddac
syuctzhaoyang <48762502+syuctzhaoyang@users.noreply.github.com>
2020-05-31 16:57:44 +0800

    Update 4predict.py

--- 卷积神经网络人脸识别/4predict.py
+++ 卷积神经网络人脸识别/4predict.py
@@ -8,13 +8,15 @@ import matplotlib.pyplot as plt
 face_cascade = cv.CascadeClassifier(r'C:\opencv4\build\etc\haarcascades\haarcascade_frontalface_default.xml')
 from PIL import Image
 from tensorflow.keras.models import load_model
-
+#加载训练好的模型及参数
 classifier = load_model('model.h5')
-
+#测试图片地址
 filename = r'prediction_dataset/i3.jpg'
+#提取人脸后的照片的地址
 file_d_name = r'prediction_dataset/i3_d.jpg'
 
 img = cv.imread(filename)
+#提取人脸
 faces = face_cascade.detectMultiScale(img, 1.3, 5)
 im = Image.open(filename)
 
@@ -31,9 +33,10 @@ test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis = 0)
 flag = classifier.predict_classes(test_image)[0];
 
-# im.show()
+#显示原有图片
 plt.imshow(im)
 plt.show()
+#输入判断结果
 if(flag == 0):
     print('范冰冰')
 elif (flag == 1):
@@ -42,3 +45,4 @@ elif(flag == 2):
     print('林志玲')
 else:
     print('我不认识这个人')
+
